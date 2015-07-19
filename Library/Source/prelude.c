e93752f03751f5627196cc4ffcb07809bec44f39
Colin Hill <colin.james.hill@gmail.com>
2015-07-19 08:10:22 -0700

    Moved Prelude to its own files

---
+++ Library/Source/prelude.c
@@ -0,0 +1,214 @@
+///////////////////////////////////////////////////////////////////////////////
+// This source file is part of Nominal.
+//
+// Copyright (c) 2015 Colin Hill
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////
+#include "prelude.h"
+
+#include <assert.h>
+
+
+static NomValue prelude_print(
+    NomState*   state)
+{
+    assert(state);
+
+    char string[8192];
+    size_t argcount = nom_getargcount(state);
+    for (size_t i = 0; i < argcount; ++i)
+    {
+        NomValue arg = nom_getarg(state, i);
+        if (nom_isstring(arg))
+        {
+            printf("%s", nom_getstring(state, arg));
+        }
+        else
+        {
+            nom_tostring(state, string, 8192, arg);
+            printf("%s", string);
+        }
+
+        if (i < argcount - 1)
+        {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
+    return nom_nil();
+}
+
+static NomValue prelude_forvalues(
+    NomState*   state)
+{
+    assert(state);
+
+    NomValue values = nom_getarg(state, 0);
+    NomValue function = nom_getarg(state, 1);
+
+    if (nom_isiterable(state, values))
+    {
+        if (nom_isinvokable(state, function))
+        {
+            NomIterator iterator = { 0 };
+            while (nom_next(state, values, &iterator))
+            {
+                NomValue value = iterator.value;
+                nom_invoke(state, function, 1, &value);
+            }
+        }
+        else
+        {
+            nom_seterror(state, "'function' is not invokable");
+        }
+    }
+    else
+    {
+        nom_seterror(state, "'values' is not iterable");
+    }
+
+    return nom_nil();
+}
+
+static NomValue prelude_forkeys(
+    NomState*   state)
+{
+    assert(state);
+
+    NomValue keys = nom_getarg(state, 0);
+    NomValue function = nom_getarg(state, 1);
+
+    if (nom_isiterable(state, keys))
+    {
+        if (nom_isinvokable(state, function))
+        {
+            NomIterator iterator = { 0 };
+            while (nom_next(state, keys, &iterator))
+            {
+                NomValue key = iterator.key;
+                nom_invoke(state, function, 1, &key);
+            }
+        }
+        else
+        {
+            nom_seterror(state, "'function' is not invokable");
+        }
+    }
+    else
+    {
+        nom_seterror(state, "'keys' is not iterable");
+    }
+
+    return nom_nil();
+}
+
+static NomValue prelude_if(
+    NomState*   state)
+{
+    assert(state);
+
+    NomValue condition = nom_getarg(state, 0);
+    NomValue thenFunction = nom_getarg(state, 1);
+    NomValue elseFunction = nom_getarg(state, 2);
+
+    NomValue result;
+    if (nom_isinvokable(state, condition))
+    {
+        result = nom_invoke(state, condition, 0, NULL);
+    }
+    else
+    {
+        result = condition;
+    }
+
+    if (!nom_error(state))
+    {
+        if (nom_istrue(state, result) && nom_istrue(state, thenFunction))
+        {
+            if (nom_isinvokable(state, thenFunction))
+            {
+                return nom_invoke(state, thenFunction, 0, NULL);
+            }
+            else
+            {
+                nom_seterror(state, "'then' is not invokable");
+            }
+        }
+        else if (!nom_istrue(state, result) && nom_istrue(state, elseFunction))
+        {
+            if (nom_isinvokable(state, elseFunction))
+            {
+                return nom_invoke(state, elseFunction, 0, NULL);
+            }
+            else
+            {
+                nom_seterror(state, "'else' is not invokable");
+            }
+        }
+    }
+
+    return nom_nil();
+}
+
+static NomValue prelude_assertequal(
+    NomState*   state)
+{
+    assert(state);
+
+    NomValue actual = nom_getarg(state, 0);
+    NomValue expected = nom_getarg(state, 1);
+
+    if (!nom_equals(state, actual, expected))
+    {
+        char actualBuffer[128];
+        nom_tostring(state, actualBuffer, 128, actual);
+        char expectedBuffer[128];
+        nom_tostring(state, expectedBuffer, 128, expected);
+
+        nom_seterror(state, "Failed assertion: %s != %s", actualBuffer, expectedBuffer);
+    }
+
+    return nom_nil();
+}
+
+void import_prelude(
+    NomState*   state
+    )
+{
+    assert(state);
+
+    nom_letvar(state, "nil", nom_nil());
+    assert(!nom_error(state));
+    nom_letvar(state, "true", nom_true());
+    assert(!nom_error(state));
+    nom_letvar(state, "false", nom_false());
+    assert(!nom_error(state));
+    nom_letvar(state, "print", nom_newfunction(state, prelude_print));
+    assert(!nom_error(state));
+    nom_letvar(state, "if", nom_newfunction(state, prelude_if));
+    assert(!nom_error(state));
+    nom_letvar(state, "assertEqual", nom_newfunction(state, prelude_assertequal));
+    assert(!nom_error(state));
+    nom_letvar(state, "forValues", nom_newfunction(state, prelude_forvalues));
+    assert(!nom_error(state));
+    nom_letvar(state, "forKeys", nom_newfunction(state, prelude_forkeys));
+    assert(!nom_error(state));
+}
--- Library/Source/state.c
+++ Library/Source/state.c
@@ -27,6 +27,7 @@
 #include "map.h"
 #include "function.h"
 #include "parser.h"
+#include "prelude.h"
 #include "codegen.h"
 #include "string.h"
 
@@ -200,169 +201,6 @@ static void invoke(
     }
 }
 
-static NomValue prelude_print(
-    NomState*   state)
-{
-    assert(state);
-
-    char string[8192];
-    size_t argcount = nom_getargcount(state);
-    for (size_t i = 0; i < argcount; ++i)
-    {
-        NomValue arg = nom_getarg(state, i);
-        if (nom_isstring(arg))
-        {
-            printf("%s", nom_getstring(state, arg));
-        }
-        else
-        {
-            nom_tostring(state, string, 8192, arg);
-            printf("%s", string);
-        }
-
-        if (i < argcount - 1)
-        {
-            printf(" ");
-        }
-    }
-    printf("\n");
-
-    return nom_nil();
-}
-
-static NomValue prelude_forvalues(
-    NomState*   state)
-{
-    assert(state);
-
-    NomValue values = nom_getarg(state, 0);
-    NomValue function = nom_getarg(state, 1);
-
-    if (nom_isiterable(state, values))
-    {
-        if (nom_isinvokable(state, function))
-        {
-            NomIterator iterator = { 0 };
-            while (nom_next(state, values, &iterator))
-            {
-                NomValue value = iterator.value;
-                nom_invoke(state, function, 1, &value);
-            }
-        }
-        else
-        {
-            nom_seterror(state, "'function' is not invokable");
-        }
-    }
-    else
-    {
-        nom_seterror(state, "'values' is not iterable");
-    }
-
-    return nom_nil();
-}
-
-static NomValue prelude_forkeys(
-    NomState*   state)
-{
-    assert(state);
-
-    NomValue keys = nom_getarg(state, 0);
-    NomValue function = nom_getarg(state, 1);
-
-    if (nom_isiterable(state, keys))
-    {
-        if (nom_isinvokable(state, function))
-        {
-            NomIterator iterator = { 0 };
-            while (nom_next(state, keys, &iterator))
-            {
-                NomValue key = iterator.key;
-                nom_invoke(state, function, 1, &key);
-            }
-        }
-        else
-        {
-            nom_seterror(state, "'function' is not invokable");
-        }
-    }
-    else
-    {
-        nom_seterror(state, "'keys' is not iterable");
-    }
-
-    return nom_nil();
-}
-
-static NomValue prelude_if(
-    NomState*   state)
-{
-    assert(state);
-
-    NomValue condition = nom_getarg(state, 0);
-    NomValue thenFunction = nom_getarg(state, 1);
-    NomValue elseFunction = nom_getarg(state, 2);
-
-    NomValue result;
-    if (nom_isinvokable(state, condition))
-    {
-        result = nom_invoke(state, condition, 0, NULL);
-    }
-    else
-    {
-        result = condition;
-    }
-
-    if (!nom_error(state))
-    {
-        if (nom_istrue(state, result) && nom_istrue(state, thenFunction))
-        {
-            if (nom_isinvokable(state, thenFunction))
-            {
-                return nom_invoke(state, thenFunction, 0, NULL);
-            }
-            else
-            {
-                nom_seterror(state, "'then' is not invokable");
-            }
-        }
-        else if (!nom_istrue(state, result) && nom_istrue(state, elseFunction))
-        {
-            if (nom_isinvokable(state, elseFunction))
-            {
-                return nom_invoke(state, elseFunction, 0, NULL);
-            }
-            else
-            {
-                nom_seterror(state, "'else' is not invokable");
-            }
-        }
-    }
-
-    return nom_nil();
-}
-
-static NomValue prelude_assertequal(
-    NomState*   state)
-{
-    assert(state);
-
-    NomValue actual = nom_getarg(state, 0);
-    NomValue expected = nom_getarg(state, 1);
-
-    if (!nom_equals(state, actual, expected))
-    {
-        char actualBuffer[128];
-        nom_tostring(state, actualBuffer, 128, actual);
-        char expectedBuffer[128];
-        nom_tostring(state, expectedBuffer, 128, expected);
-
-        nom_seterror(state, "Failed assertion: %s != %s", actualBuffer, expectedBuffer);
-    }
-
-    return nom_nil();
-}
-
 NomState* nom_newstate(
     void
     )
@@ -378,23 +216,7 @@ NomState* nom_newstate(
     // Global scope
     PUSH_FRAME(0, 0, nom_newmap(state));
 
-    // Declare intrinsic variables
-    nom_letvar(state, "nil", nom_nil());
-    assert(!nom_error(state));
-    nom_letvar(state, "true", nom_true());
-    assert(!nom_error(state));
-    nom_letvar(state, "false", nom_false());
-    assert(!nom_error(state));
-    nom_letvar(state, "print", nom_newfunction(state, prelude_print));
-    assert(!nom_error(state));
-    nom_letvar(state, "if", nom_newfunction(state, prelude_if));
-    assert(!nom_error(state));
-    nom_letvar(state, "assertEqual", nom_newfunction(state, prelude_assertequal));
-    assert(!nom_error(state));
-    nom_letvar(state, "forValues", nom_newfunction(state, prelude_forvalues));
-    assert(!nom_error(state));
-    nom_letvar(state, "forKeys", nom_newfunction(state, prelude_forkeys));
-    assert(!nom_error(state));
+    import_prelude(state);
 
     return state;
 }
