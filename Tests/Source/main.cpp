59b891acd82b074e7a3c006f12e8732dfed4bab0
Colin Hill <colin.james.hill@gmail.com>
2015-07-23 20:56:25 -0700

    Moved Catch header to the Dependencies directory

--- Tests/Source/main.cpp
+++ Tests/Source/main.cpp
@@ -27,4 +27,4 @@ extern "C"
 }
 
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include <catch.hpp>
--- Tests/Source/negativetests.cpp
+++ Tests/Source/negativetests.cpp
@@ -21,7 +21,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 ///////////////////////////////////////////////////////////////////////////////
-#include "catch.hpp"
+#include <catch.hpp>
 
 extern "C"
 {
